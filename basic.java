c34c42b15dd0f8e509f84ad69b826ee4b4775239
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-16 22:39:21 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,13 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class solution {
+    public static void main(String[] args) {
+        //Java Collection Framework
+        //like oops -> ClassName objectName=new ClassName();
+        ArrayList<Integer> ch = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+    }
+}
