a50affd3e566b66dc5ca034b477e49b494665e7c
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-16 22:55:51 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -1,11 +1,36 @@
 import java.util.ArrayList;
-
 public class solution {
     public static void main(String[] args) {
         //Java Collection Framework
         //like oops -> ClassName objectName=new ClassName();
-        ArrayList<Integer> ch = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         ArrayList<String> list2 = new ArrayList<>();
         ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list.add(1); //O(1)
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        list.add(1,9); //O(n)
+
+        System.out.println(list);
+
+        //Get Operation - O(1)
+        int element=list.get(2);
+        System.out.println(element);
+
+        //Delete - O(n)
+        list.remove(2);
+        System.out.println(list);
+
+        //Set - O(n)
+        list.set(2,10);
+        System.out.println(list);
+
+        //contains - O(n) 
+        System.out.println(list.contains(1));
+        System.out.println(list.contains(11));
+
     }
 }
